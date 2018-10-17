c177faefb5bfc383a380e3802bc0f2801004b19f
Lily Johnson <lilyj521@gmail.com>
2018-10-17 15:36:48 -0600

    added circles

--- coursework/hw-8/sketch.js
+++ coursework/hw-8/sketch.js
@@ -6,8 +6,27 @@ createCanvas(windowWidth,windowHeight);
 function draw() {
   background('rgb(209, 168, 255)')
 
-for (let i = 0; i <= width; i += 119) {
+for (let x = 0; x <= width; x += 119) {
+  for (let y = 0; y <= height; y += 133) {
   noStroke()
-  ellipse(i, 100, 75, 75)
-}
+  ellipse(x, y, 75, 75)
+    }
+  }
+push()
+  for (let i = 5; i <= width; i += 119) {
+    for (let e = 5; e <= height; e += 264) {
+      fill('blue')
+      ellipse(i, e, 50, 50)
+    }
+  }
+  pop()
+
+  push()
+  for (let i = -5; i <= width; i += 119) {
+    for (let e = 133; e <= height; e += 266) {
+      fill('blue')
+      ellipse(i, e, 50, 50)
+    }
+  }
+  pop()
 }
